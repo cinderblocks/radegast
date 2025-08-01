701c63a832b3fcbd94d448c887fbb4ef3f9ee67c
TsengSR <tsengsr@example.com>
2025-08-01 15:43:52 +0200

    Added WearableTexture component to preview/display wearables

---
+++ Radegast/GUI/Consoles/Assets/WearableTextures.Designer.cs
@@ -0,0 +1,118 @@
+/*
+ * Radegast Metaverse Client
+ * Copyright(c) 2009-2014, Radegast Development Team
+ * Copyright(c) 2016-2025, Sjofn, LLC
+ * All rights reserved.
+ *
+ * Radegast is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.If not, see<https://www.gnu.org/licenses/>.
+ */
+
+namespace Radegast
+{
+    partial class WearableTextures
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.pnlImages = new System.Windows.Forms.Panel();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.pnlTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Getting textures for: ";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(115, 14);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(37, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "name";
+            // 
+            // pnlImages
+            // 
+            this.pnlImages.AutoScroll = true;
+            this.pnlImages.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlImages.Location = new System.Drawing.Point(0, 41);
+            this.pnlImages.MaximumSize = new System.Drawing.Size(5000, 0);
+            this.pnlImages.Name = "pnlImages";
+            this.pnlImages.Size = new System.Drawing.Size(489, 351);
+            this.pnlImages.TabIndex = 1;
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.Controls.Add(this.label1);
+            this.pnlTop.Controls.Add(this.lblName);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(489, 41);
+            this.pnlTop.TabIndex = 2;
+            // 
+            // OutfitTextures
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlImages);
+            this.Controls.Add(this.pnlTop);
+            this.Name = "OutfitTextures";
+            this.Size = new System.Drawing.Size(489, 392);
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label lblName;
+        public System.Windows.Forms.Panel pnlImages;
+        public System.Windows.Forms.Panel pnlTop;
+
+    }
+}
